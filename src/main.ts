ddb301c5a5a9babe71a858c83f0a80d49733ef5f
Almazatun <atunstyle@gmail.com>
2021-10-14 17:01:33 +0300

    Simple refactor file

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,13 @@
-const a: string = 'Almaz';
+import { createBitMapInput, createBitmaps } from './create-bit-maps';
 
-console.log(a)
+const list: createBitMapInput[] = [
+  {
+    lineSize: 3,
+    columnSize: 4,
+    pixels: '0001,0010,0110',
+  },
+];
+
+const createBitMaps = createBitmaps(list);
+
+console.log('------>', createBitMaps);
