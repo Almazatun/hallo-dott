ef6a313504d75e0efdb0fb8b978b0138d46ce04f
Almazatun <atunstyle@gmail.com>
2021-10-14 09:43:07 +0300

    Add const values

---
+++ src/types.ts
@@ -0,0 +1,14 @@
+export enum CONST_PIXEL_COLOR {
+  BLACK = 0,
+  WHITE = 1
+}
+
+export enum CONST_BITMAP_SIZE {
+  MIN = 1,
+  MAX = 182
+}
+
+export enum CONST_NUMBER_TEST {
+    MIN = 1,
+    MAX = 1000
+}
