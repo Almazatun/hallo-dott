fb05507722aefda4ab8b91891b650bc71681bdfd
Almazatun <atunstyle@gmail.com>
2021-10-14 22:10:11 +0300

    Add function to read txt file. *processLineByLine

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,61 @@
+import fs from 'fs';
+import readline from 'readline';
 import { createBitMapInput, createBitmaps } from './create-bit-maps';
 
-const list: createBitMapInput[] = [
-  {
-    lineSize: 3,
-    columnSize: 4,
-    pixels: '0001,0010,0110',
-  },
-];
+// __dirname means relative to script. Use "./data.txt" if you want it relative to execution path.
 
-const createBitMaps = createBitmaps(list);
+const listBitMaps: createBitMapInput[] = [];
 
-console.log('------>', createBitMaps);
+async function processLineByLine() {
+  const fileStream = fs.createReadStream('input.txt');
+
+  const rl = readline.createInterface({
+    input: fileStream,
+    crlfDelay: Infinity,
+    output: process.stdout,
+  });
+
+  rl.on('line', (line) => {
+    // if (line === '') {
+    //   return null;
+    // }
+
+    try {
+      console.log(`Line from file: ${line.trim()}`);
+      if (line.includes(' ')) {
+        const [lineSize, columnSize] = line.split(' ');
+        console.log('==>', lineSize);
+        console.log('==>', columnSize);
+
+        const bitMap: createBitMapInput = {
+          lineSize: Number(lineSize),
+          columnSize: Number(columnSize),
+          pixels: '',
+        };
+
+        listBitMaps.push(bitMap);
+      }
+      if (!line.includes(' ')) {
+        if (listBitMaps[listBitMaps.length - 1] !== undefined) {
+          console.log('==>PIXELS', line);
+          console.log('==>PIXELS_1', listBitMaps[listBitMaps.length - 1]);
+          const lineArr = [...line.trim()];
+
+          if (lineArr.length === listBitMaps[listBitMaps.length - 1].columnSize) {
+            if (listBitMaps[listBitMaps.length - 1].pixels === '') {
+              listBitMaps[listBitMaps.length - 1].pixels = line;
+            } else {
+              listBitMaps[listBitMaps.length - 1].pixels += `,${line}`;
+            }
+          }
+        }
+      }
+    } catch (error) {
+      throw new Error(`Line parsing error: ${error}`);
+    }
+  });
+}
+(async () => {
+  await processLineByLine();
+  console.log('result', listBitMaps);
+})();
